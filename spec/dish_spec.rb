016402ddf69c3283adef1e2b46d52d88f18366db
Sanda Golcea <sanda.golcea@gmail.com>
2014-12-22 12:21:45 +0000

    dish, menu: basic spec tests; updates readme

---
+++ spec/dish_spec.rb
@@ -0,0 +1,10 @@
+require 'dish'
+
+describe Dish do
+  it 'should have a name' do
+  end
+
+  it 'should have a price' do
+  end
+
+end
---
+++ spec/menu_spec.rb
@@ -0,0 +1,14 @@
+require 'menu'
+
+describe Menu do
+  
+  it 'should have a name' do
+  end
+
+  it 'should have a list of dishes' do
+  end
+
+  it 'should be able to count its dishes' do
+  end
+
+end
